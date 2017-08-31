705c75db6ba7fb007508bcd5f44dd7dbc5e49ad9
Matias Ruiz Moreno <matiasrm14@gmail.com>
2017-08-30 21:58:05 -0300

    Le agregue los estilos y los script al welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,54 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-<style>
-    html,body{
-        font-family: Arial,sans-serif;
-        height: 100%;
-        margin: 0;
-        padding: 0;
-    }
-    #map{
-        height:100%;
-    }
-</style>
-    <title></title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SISTEMA FARMACIA</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/miEstilo.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="{{asset('css/freelancer.min.css')}}" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="{{asset('vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+      <!--<link href="{{asset('css/home.css')}}" rel="stylesheet"> -->
+      <link href="{{asset('css/bxslider.css')}}" rel="stylesheet">
+
+      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
 </head>
 <body>
 <div id="map">
 </div>
-<script>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/freelancer.min.js"></script>
+    <script src="js/bxslider.js"></script>  
+
+    <script>
     var map;
     var markers=[];
 
